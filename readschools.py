7fcb23625efda4c4f220316cbad946783a1dd144
Seth Woodworth <seth@isforinsects.com>
2010-03-25 14:31:13 -0400

    tool for parsing csvs and generating wiki URL list for final tool

---
+++ readschools.py
@@ -0,0 +1,9 @@
+#!/bin/python
+import csv
+import os
+
+wikid = csv.DictReader(open('./nocheckin/CodedWikis-1798.csv', 'r'), delimiter=';', quotechar='"')
+wikil = []
+
+for row in wikid:
+    wikil.append(row['wikiname']);
