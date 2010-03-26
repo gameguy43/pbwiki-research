0140dac3563fc031005599bc21c45088fedaa899
Seth Woodworth <seth@isforinsects.com>
2010-03-26 16:34:19 -0400

    stores first level stats and some FrontPage deets

--- pbwiki.py
+++ pbwiki.py
@@ -39,14 +39,14 @@ class PBWiki(object):
       f.close()
   
     # PB Wiki API is documented at https://some_wiki_name.pbwiki.com/api_v2/
-    def _api_call(self, oper, **args):
+    def _api_call(self, oper, **kwargs):
       """Make an API call against a pbwiki site.
       oper - the operation to perform
       ** any other keyword arguments are passed on to the API call
       """
       # Generate an appropriate API call URL
-      args['_type'] = 'jsontext'
-      argstr = '/'.join(["%s/%s" % (k,v) for k, v in args.iteritems()])
+      kwargs['_type'] = 'jsontext'
+      argstr = '/'.join(["%s/%s" % (k,v) for k, v in kwargs.iteritems()])
       call_url = '%s/api_v2/op/%s/%s' % (self.url, oper, argstr)
   
       # Fetch text and strip off first and last lines, which are comment tags
@@ -85,6 +85,10 @@ class PBWiki(object):
         wiki_files = self._api_call('GetFiles')
         return wiki_files
 
+    def get_page(self, url):
+        wiki_page = self._api_call('GetPage', page='FrontPage')
+        return wiki_page
+
     def get_pages(self, url):
         wiki_pages = self._api_call('GetPages')
         return wiki_pages
@@ -95,57 +99,73 @@ class PBWiki(object):
 
 ## Above is a PBWiki class
 ## Below is code for traversing, calling and storing wiki data
+def parse_page(json):
+    #print json
+    page_dict = {
+        'page_checked_at' : json['_valid_as_of'], # somewhat obsoleted by page_url which has rev timestamp
+        'page_url' : json['revurl'], # persistant url of checked page
+        'page_name' : json['name'], # often 'FrontPage' in current implementattion
+        'page_revision' : json['revcount'], # int
+        'page_char_size' : json['size'], # int character count
+        'page_html' : json['html'] # html for analysis, tokenize/strip-formating somewhere else
+    }
+    return page_dict
+
 def parse_files(json):
-    # {"_auth_role":"admin","_auth_via":"cookie","_optimized_paging":true,"_perm_cache_times":{"foldertime":1269477576,"filetime":1269477576,"permtime":1269477575},"_total_count":0,"_valid_as_of":1269586477,"files":[]}
+    #print json
     files_dict = {
-        'files_checked_at' : json['_valid_as_of'],
+        'files_checked_at' : json['_valid_as_of'], # handy to capture when this was checked for rolling updates
         'files_count' : json['_total_count'],
-        'file_list' : json['files'],
+        'files_list' : json['files'],
     }
     return files_dict
 
 def parse_times(json):
     times_dict = {
         'times_checked_at' : json['_valid_as_of'],
-        'comment_time' : json['commenttime'],
-        'file_time' : json['filetime'],
-        'pageedit_time' : json['pagetime'],
-        'permission_time' : json['permtime'],
-        'tag_time' : json['tagtime']
+        'time_comment' : json['commenttime'], # timestamp of last comment
+        'time_file' : json['filetime'], # timestamp of last uploaded(?) file
+        'time_pageedit' : json['pagetime'], # last edit/move/create page
+        'time_permission' : json['permtime'], # last permission edit
+        'time_tag' : json['tagtime'] # last tag added/edited/removed
     }
     return times_dict
-    
 
 def parse_pages(json):
     # Take json from get_pages, turn into nice dict
-    pages_count = json['_total_count']
     pages = json['pages']
     pages_list = []
     for i in pages:
         pages_list.append(i['name'])
-    
     wiki_dict = { 
-        'pages_count': pages_count,
-        'pages': pages_list
+        'pages_count': json['_total_count'], # number of pages in wiki
+        'pages_list': pages_list # make sure list is not problematic
     }
     return wiki_dict
 
 def examine_wiki(url):
-    # Make api call to get pages data
-    # this should prob make several api calls and parse them, not pass results
+    # Makes several api calls to get wiki data
+    # returns ???
     myswiki = PBWiki(url)
-    # get files and store
+    # get_files and store
     files_json = myswiki.get_files(url)
     files_dict = parse_files(files_json)
-    print files_dict
+    #print files_dict
     # get_pages and store
     pages_json = myswiki.get_pages(url)
-    page_dict = parse_pages(pages_json)
+    pages_dict = parse_pages(pages_json)
+    #print page_dict
     # get_times and store
     times_json = myswiki.get_times(url)
     times_dict = parse_times(times_json)
-    print times_dict
-    return page_dict
+    #print times_dict
+    # get_page and store
+    page_json = myswiki.get_page(url)
+    page_dict = parse_page(page_json)
+    #print page_dict
+    
+    data_dict = dict(pages_dict.items() + files_dict.items() + times_dict.items() + page_dict.items())
+    return data_dict
 
 def get_wikilist():
     # Read the semi-private list of wikis from file
@@ -163,6 +183,7 @@ def glue():
     test_url = 'http://testapi.pbworks.com'
     wiki_data = examine_wiki(test_url)
     print wiki_data
+    return wiki_data
     
 if __name__ == '__main__':
-    glue()
+    wd = glue()
